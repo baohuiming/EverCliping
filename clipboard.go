8a786271cf4e7c0a43019b66a2017387ee182076
Bao Huiming <m@baohuiming.net>
2025-03-24 19:39:46 +0800

    feat: lock

--- clipboard.go
+++ clipboard.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"sync"
 
 	"golang.design/x/clipboard"
 )
@@ -16,6 +17,7 @@ var (
 	ClipboardText   *string
 	ClipboardImage  *[]byte
 	ClipboardLatest string = ""
+	ClipboardMu     sync.Mutex
 )
 
 func ClipboardInit() {
@@ -31,26 +33,34 @@ func ClipboardInit() {
 func WatchText(ctx context.Context) {
 	ch := clipboard.Watch(ctx, clipboard.FmtText)
 	for data := range ch {
+		ClipboardMu.Lock()
 		text := string(data)
 		log.Println("Text data:", text)
 		*ClipboardText = text
 		ClipboardLatest = TypeText
+		ClipboardMu.Unlock()
 	}
 }
 
 func WatchImage(ctx context.Context) {
 	ch := clipboard.Watch(ctx, clipboard.FmtImage)
 	for data := range ch {
+		ClipboardMu.Lock()
 		log.Println("Image data:", len(data))
 		*ClipboardImage = data
 		ClipboardLatest = TypeImage
+		ClipboardMu.Unlock()
 	}
 }
 
 func SetClipboardText(text string) {
+	ClipboardMu.Lock()
+	defer ClipboardMu.Unlock()
 	clipboard.Write(clipboard.FmtText, []byte(text))
 }
 
 func SetClipboardImage(data []byte) {
+	ClipboardMu.Lock()
+	defer ClipboardMu.Unlock()
 	clipboard.Write(clipboard.FmtImage, data)
 }
